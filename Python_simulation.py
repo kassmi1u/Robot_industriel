99eed69c5c8f504dc48fa80b81f3f828fe2a6310
kassmi1u <56152551+kassmi1u@users.noreply.github.com>
2021-04-30 19:59:37 +0000

    Simulation OK

--- Python_simulation.py
+++ Python_simulation.py
@@ -3,6 +3,7 @@
 import numpy as np
 import scipy as sp
 import sympy as sp
+import math
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
@@ -18,139 +19,61 @@ from sympy.physics.mechanics import dynamicsymbols, Point, ReferenceFrame
 
 class Robot_manipulator : 
 
-    def __init__(self,th1,th2):
-        self.theta1s = th1
-        self.theta2s = th2
+    def __init__(self):
+        self.theta1s = 0
+        self.theta2s = 0
         self.L1 = 0.5
         self.L2 = 0.5
 
-
-    def set_theta(self,w1,w2) : 
-        self.theta1s = w1*60
-        self.theta2s = w2*60
+    def set_theta(self,nv_th1,nv_th2) : 
+        self.theta1s = nv_th1
+        self.theta2s = nv_th2
     
     def get_theta(self): 
         return self.theta1s, self.theta2s
 
+    def get_coord_coude(self) : 
+        return [self.L1 * math.cos(self.theta1s), self.L1 * math.sin(self.theta1s)]
+    
+    def get_coord_pince(self):
+        return [self.L1 * math.cos(self.theta1s)+
+                self.L2 * math.cos(self.theta1s + self.theta2s),
+                self.L1 * math.sin(self.theta1s)+
+                self.L2 * math.sin(self.theta1s + self.theta2s)]
 
-    #Repères et points
-    def set_paramater(self,Theta1s,Theta2s):
+    def generate_liste_of_coord(self,th1,th2) : 
+        liste1 = []
+        liste2 = []
+        for i in range(len(th1)) : 
+            self.set_theta(th1[i],th2[i])
+            liste1.append(self.get_coord_coude())
+            liste2.append(self.get_coord_pince())
+        return liste1, liste2
 
-        print("Définition des paramêtres (sous forme de symbol)")
-        l1, l2 = sp.symbols('l1 l2')
-        print("Parametres: ",l1,l2)
-        l1,l2
+    def train(self,th1,th2):
 
-        print("Définition des DDL, variables fonction du temps") 
-        theta1, theta2 = dynamicsymbols('theta1 theta2')
-        print("DDL: ",theta1,theta2)
-        theta1,theta2
-
-        #repères
-        RO = ReferenceFrame('R_O')
-        RP = ReferenceFrame('R_P')
-        RQ = ReferenceFrame('R_Q')
-
-        RP.orient(RO, 'Axis', [theta1, RO.z])
-        RQ.orient(RP, 'Axis', [theta2, RO.z])
-
-        T02 = RO.dcm(RQ)
-        T02.simplify()
-
-        #Points
-        O = Point('O')
-        P = Point('P')
-        Q = Point('Q')
-
-        P.set_pos(O, l1 * RP.x)
-        Q.set_pos(P, l2 * RQ.x)
-
-        #position de Q dans le repère de base
-        Q.pos_from(O)
-
-        qxy = (Q.pos_from(O).express(RO)).simplify()
-        qxy
-
-        #composantes de Q suivant x et y dans le repère RO
-        qx=qxy.dot(RO.x)
-        qx
-
-        qy=qxy.dot(RO.y)
-        qy
-
-        #idem pour le point P
-        pxy = (P.pos_from(O).express(RO)).simplify()
-        pxy
-
-        #composante de Q dans RO
-        px = pxy.dot(RO.x)
-        py = pxy.dot(RO.y)
-
-        ######Simulation géométrique
-        Qx = sp.lambdify((l1, l2, theta1, theta2), qx, 'numpy')
-        Qy = sp.lambdify((l1, l2, theta1, theta2), qy, 'numpy')
-        Px = sp.lambdify((l1, theta1), px, 'numpy')
-        Py = sp.lambdify((l1, theta1), py, 'numpy')
-
-        #application numérique
-        # nbre de valeurs de theta
-        Np=100
-        # longueur du bras en m
-        L1 = 0.5
-        L2 = 0.5
-        # variation des angles
-        theta1s = Theta1s
-        theta2s = Theta2s
-        # calcul de la positions fct angle
-        QX = np.array(Qx(L1, L2, theta1s, theta2s))
-        QY = np.array(Qy(L1, L2, theta1s, theta2s))
-        PX = np.array(Px(L1,theta1s))
-        PY = np.array(Py(L1,theta1s))
-        return PX,PY,QX,QY
-
-
-    def get_coord_pince(self) : 
-        P1,P2,P3,P4 = self.set_paramater(self.theta1s,self.theta2s)
-        return P3,P4
-
-    #Animation de la trajectoire
-    def env_animation(self,L1,L2) : 
-
-        #fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6))
+        P,Q = self.generate_liste_of_coord(th1,th2)
         Fig=plt.figure(figsize=(8,8))
         ax = Fig.add_subplot(111, aspect='equal')
-        ax.set_axis_off()
-        ax.set_xlim((-1.2*(L1+L2),1.2*(L1+L2)))
-        ax.set_ylim((-1.2*(L1+L2),1.2*(L1+L2)))
+        ax.set_xlim((-1.2*(self.L1+self.L2),1.2*(self.L1+self.L2)))
+        ax.set_ylim((-1.2*(self.L1+self.L2),1.2*(self.L1+self.L2)))
         ax.set_title('mouvement du bras de robot',fontsize=30)
-        #fig.set_facecolor("#ffffff")
-        line1, = ax.plot([0.,L1], [0.,0.], 'o-b', lw=18 , markersize=25)
-        line2, = ax.plot([L1,L1+L2], [0.,0.], 'o-', lw=18 , markersize=25)
-        pt1    = ax.scatter([L1+L2+0.05],[0.],marker="$\in$",s=800,c="black",zorder=3)
-        return Fig,line1,line2,pt1
-
-
-    def train(self): 
-        #Np =100
-        
-        x1,x2,x3,x4 = self.env_animation(self.L1,self.L2)
-        
+        line1, = ax.plot([0.,self.L1], [0.,0.], 'o-b', lw=18 , markersize=25)
+        line2, = ax.plot([self.L1,self.L1+self.L2], [0.,0.], 'o-', lw=18 , markersize=25)
+        pt1    = ax.scatter([self.L1+self.L2],[0.],marker="$\in$",s=800,c="black",zorder=3)
 
         def animate(i):
-            global PX,PY,QX,QY
-            #THeta1s = np.linspace(0, np.pi,100)
-            #THeta2s = np.linspace(0, np.pi/2,100)
-            PX,PY,QX,QY = self.set_paramater(self.theta1s,self.theta2s)
-            x2.set_data([0.,PX[i]],[0.,PY[i]])
-            x3.set_data([PX[i],QX[i]],[PY[i],QY[i]])
-            x4.set_offsets([QX[i]+0.05,QY[i]])
-            return x2,x3,x4
+            line1.set_data([0.,P[i][0]],[0.,P[i][1]])
+            line2.set_data([P[i][0],Q[i][0]],[P[i][1],Q[i][1]])
+            pt1.set_offsets([Q[i][0],Q[i][1]])
+            return line1,line2,pt1
 
-        anim = animation.FuncAnimation(x1, animate, np.arange(1, 100), interval=50, blit=True)
+        anim = animation.FuncAnimation(Fig, animate, np.arange(1, len(th1)), interval=50, blit=True)
         plt.show()
 
-#"""th1 = np.linspace(0, np.pi,100)   # valeur rad
-#th2 = np.linspace(0, np.pi/2,100)
-#longg = 0.5
-#bra = Robot_manipulator(th1,th2,longg)
-#bra.train()"""
+#Test 
+th1 = np.linspace(0, np.pi,100)
+th2 = np.linspace(0, np.pi/2,100)
+longg = 0.5
+bra = Robot_manipulator()
+bra.simulate(th1,th2)

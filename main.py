273b88274d96b4eb66cabcd0992ae119335700bd
kassmi1u <56152551+kassmi1u@users.noreply.github.com>
2021-05-06 23:59:08 +0000

    add 3ddl model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from online_trainer import OnlineTrainer
+from online_trainer_2ddl import OnlineTrainer_2ddl
+from online_trainer_3ddl import OnlineTrainer_3ddl
 from BackProp_Python_v2 import NN
-from Python_simulation import Robot_manipulator
+from simulation_2ddl import Robot_manipulator_2ddl
+from simulation_3ddl import Robot_manipulator_3ddl
 import numpy as np
 import time 
 import math
@@ -29,7 +31,6 @@ root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_c
 style = ttk.Style(root)
 root.tk.call('source', 'azure.tcl')
 style.theme_use('azure')
-
 options = ['', 'OptionMenu', 'Value 1', 'Value 2']
 
 # Variable values
@@ -37,23 +38,39 @@ load_model = tk.IntVar()
 learning = tk.IntVar()
 target_x = tk.DoubleVar()
 target_y = tk.DoubleVar()
-hl_size = tk.IntVar()
+hidden = tk.IntVar()
+choise = tk.IntVar()
+step = tk.DoubleVar()
+moment = tk.DoubleVar()
 
+moment.set(0.00)
+step.set(0.5)
+choise.set(1)
+load_model.set(1)
+learning.set(1)
+hidden.set(20)
 
 
 # Network Settings zone
 frame2 = ttk.LabelFrame(root, text=' Network Settings ', width=370, height=110)
 frame2.place(x=23, y=45)
-"""
-# target_y text
-Label1 = ttk.Label(root, text = 'Hidden Layer size')
-Label1.place (x=50,y=80)
 
+# Step text
+Label1 = ttk.Label(root, text = ' Step ')
+Label1.place (x=195,y=80)
 
-# Hidden Layer size value
-boite1 = ttk.Spinbox(root,from_=5,to=100,increment=1,textvariable=hl_size,width=5)
-boite1.place(x=300,y=78)
-"""
+# Step value
+boite21 = ttk.Spinbox(root,from_=0.05,to=5,increment=0.05,textvariable=step,width=5)
+boite21.place(x=180,y=106)
+
+# Moment text
+Label1 = ttk.Label(root, text = ' Moment ')
+Label1.place (x=293,y=80)
+
+# Moment value
+boite21 = ttk.Spinbox(root,from_=0.0,to=5,increment=0.05,textvariable=moment,width=5)
+boite21.place(x=285,y=106)
+
 # Network zone
 frame1 = ttk.LabelFrame(root, text=' Network ', width=370, height=110)
 frame1.place(x=23, y=183)
@@ -88,22 +105,60 @@ Label2.place (x=50,y=398)
 boite4 = ttk.Spinbox(root,from_=0.05,to=0.95,increment=0.05,textvariable=target_y,width=5)
 boite4.place(x=300,y=393)
 
-#put an image 
+# Robot model zone
+frame21 = ttk.LabelFrame(root, text='  Robot model  ', width=170, height=110)
+frame21.place(x=413, y=183)
+
+# Robot model choise
+choise1 = ttk.Radiobutton(frame21, text=' Bras à 2DDL', variable=choise, value=1)
+choise1.place(x=20, y=15)
+choise2 = ttk.Radiobutton(frame21, text=' Bras à 3DDL', variable=choise, value=2)
+choise2.place(x=20, y=45)
+
+# Hidden Layer text
+Label1 = ttk.Label(root, text = 'Hidden Layer size')
+Label1.place (x=50,y=80)
+
+# Hidden Layer size value
+boite1 = ttk.Spinbox(root,from_=5,to=100,increment=1,textvariable=hidden,width=5)
+boite1.place(x=65,y=106)
 
 
-robot = Robot_manipulator()
-HL_size= 20 #int(hl_size.get())
+
+    
+     
+HL_size = int(hidden.get())
 network = NN(2,HL_size,2)
-training = OnlineTrainer(robot,network)
+robot = Robot_manipulator_2ddl()
+training= OnlineTrainer_2ddl(robot,network)
 thetas1 =[]
 thetas2 =[]
+thetas3 =[]
 file='last_w.json'
 
 def train_network() :
-    global thetas1,thetas2,network,HL_size,file,training,load_model,learning,target_x,target_y
-    #choice = input('Do you want to load previous network? (y/n) --> ')
-    if load_model == 1:
+    global thetas1,thetas2,thetas3,network,HL_size,file,training,load_model,learning,target_x,target_y,network,robot,step,moment
+
+    HL_size = int(hidden.get())
+    print(HL_size)
+    network = NN(2,HL_size,2)
+    robot_2ddl = Robot_manipulator_2ddl()
+    robot_3ddl = Robot_manipulator_3ddl()   
+    training_2ddl = OnlineTrainer_2ddl(robot_2ddl,network)
+    training_3ddl= OnlineTrainer_3ddl(robot_3ddl,network) 
+
+    if int(choise.get()) == 1 : 
+        robot = robot_2ddl
+        training = training_2ddl
         
+    else :
+        robot = robot_3ddl
+        training = training_3ddl
+
+    training.pas = float(step.get())
+    training.moment = float(moment.get())
+
+    if int(load_model.get()) == 1:
         with open(file) as fp:
             json_obj = json.load(fp)
         for i in range(2):
@@ -114,12 +169,17 @@ def train_network() :
                 network.wo[i][j] = json_obj["output_weights"][i][j] 
     
     target = [target_x,target_y]
-    
-    if learning == 1:
+
+    if int(learning.get()) == 1:
         training.training = True
-    elif learning == 0:
-        training.training = False   
-    thetas1,thetas2 = training.train(target)
+    elif int(learning.get()) == 0:
+        training.training = False  
+
+    if int(choise.get()) == 1 :
+        thetas1,thetas2 = training.train(target)
+    else :
+        thetas1,thetas2,thetas3 = training.train(target)
+
     json_obj = {"input_weights": network.wi, "output_weights": network.wo}
     with open(file, 'w') as fp:
         json.dump(json_obj, fp)
@@ -129,11 +189,16 @@ def train_network() :
 
 
 def animate() : 
-    global thetas1,thetas2,target_x,target_y
+    global thetas1,thetas2,thetas3,target_x,target_y,robot
     tar = [float(target_x.get()),float(target_y.get())]
     Fig, ax = robot.draw_env(tar)
-    line1, line2, pt1 = robot.draw_robot(Fig,ax)
-    robot.train(thetas1,thetas2,line1,line2,pt1,Fig)    
+    if int(choise.get()) == 1 :
+        line1, line2, pt1 = robot.draw_robot(Fig,ax)
+        robot.train(thetas1,thetas2,line1,line2,pt1,Fig) 
+    else :
+        line1,line2,line3, pt1 = robot.draw_robot(Fig,ax)
+        robot.train(thetas1,thetas2,thetas3,line1,line2,line3,pt1,Fig) 
+       
 
 def quit():
     root.destroy()
@@ -144,6 +209,8 @@ def Reset():
     target_y.set(0.00)
     learning.set(0)
     load_model.set(0)
+    choise.set(1)
+    hidden.set(20)
 
 button1= ttk.Button(root, text='Train', width=18, command=train_network)
 button1.place(x=23, y=470)
---
+++ online_trainer_2ddl.py
@@ -0,0 +1,118 @@
+import time
+import math
+
+"""def theta_s(x,y):
+    if x>0:
+        return 1*math.atan(1*y)
+    if x<=0:
+        return 1*math.atan(-1*y)"""
+
+class OnlineTrainer_2ddl:
+    
+    def __init__(self,robot,NN):
+
+        """
+        Args:
+            robot (Robot): a robot instance following the pattern of
+                VrepPioneerSimulation
+            target (list): the target position [x,y,theta]
+        """
+        self.robot = robot
+        self.network = NN
+        self.alpha = [1,1]  # normalition avec limite du monde cartesien = -3m � + 3m
+        self.training = False
+        self.running = True
+        self.moment = 0
+        self.pas = 0.5
+
+    def train(self, targett):
+        
+        position = self.robot.get_coord_pince()
+        alpha_1 = self.alpha[0]
+        alpha_2 = self.alpha[1]
+        th1 = []
+        th2 = []
+        position_x = []
+        position_y = []
+        position_x.append(position[0])
+        position_x.append(3)
+        position_y.append(position[1])
+        position_y.append(1)
+        target = [float(targett[0].get()),float(targett[1].get())]
+        print(target)
+        """position_x = []
+        position_y = []
+        position_x.append(position[0])
+        position_x.append(0)
+        position_y.append(position[1])
+        position_y.append(1) """
+        
+        #network_input[0] = 
+        #network_input[1] = 
+        robot_a_bouge = time.time()
+        i=0
+        while abs(position[0]-target[0]) > 0.001 or  abs(position[1]-target[1]) > 0.001 : 
+            """alpha_1 = 1/(max(position_x) - min(position_x))
+            alpha_2 = 1/(max(position_y) - min(position_y))"""
+            debut = time.time()
+            network_input = [(position[0]-target[0])*self.alpha[0], (position[1]-target[1])*self.alpha[1]]
+            command = self.network.runNN(network_input) # propage erreur et calcul vitesses roues instant t  # Fonction à changer
+            crit_av= alpha_1*(position[0]-target[0])*(position[0]-target[0]) + alpha_2*(position[1]-target[1])*(position[1]-target[1]) 
+
+            #alpha_x = 1/6 #"""(max(position_x) - min(position_x))"""
+            #alpha_y = 1/6 #"""(max(position_y) - min(position_y))"""   
+            
+                                 
+            diff = time.time() - robot_a_bouge
+            thetas = self.robot.get_theta()
+            theta_temp1 = thetas[0] + command[0] * diff
+            theta_temp2 = thetas[1] + command[1] * diff
+            self.robot.set_theta(theta_temp1,theta_temp2)    
+            robot_a_bouge = time.time()  
+            th1.append(theta_temp1)
+            th2.append(theta_temp2)
+            #self.robot.train(th1,th2)
+            # applique vitesses roues instant t,       # Fonction à changer               
+            time.sleep(0.050) # attend delta t
+
+            position = self.robot.get_coord_pince() #  obtient nvlle pos robot instant t+1       # Fonction à changer   
+            """position_x.append(position[0])
+            position_y.append(position[1]) """   
+            #position_x.append(position[0])
+            #position_y.append(position[1])            
+            network_input[0] = (position[0]-target[0])*self.alpha[0]
+            network_input[1] = (position[1]-target[1])*self.alpha[1]
+            #network_input[2] = (position[2]-target[2]-theta_s(position[0], position[1]))*self.alpha[2]
+            i+=1
+            print(i)
+            crit_ap= alpha_1*(position[0]-target[0])*(position[0]-target[0]) + alpha_2*(position[1]-target[1])*(position[1]-target[1]) 
+            selfthetas1,selfthetas2 = self.robot.get_theta()
+            print("manich hna")
+            if self.training:
+                delta_t = (time.time()-debut)
+                print("ani hna")
+
+                grad = [
+                    2*(-1)*alpha_1*delta_t*(self.robot.L1*math.sin(selfthetas1)+self.robot.L2*math.sin(selfthetas1 + selfthetas2))*(target[0] - position[0])
+                    -2*(-1)*alpha_2*delta_t*(self.robot.L1*math.cos(selfthetas1)+ self.robot.L2*math.cos(selfthetas1 + selfthetas2))*(target[1] - position[1]),
+                    
+                    2*(-1)*alpha_1*delta_t*(self.robot.L2*math.sin(selfthetas1+ selfthetas2))*(target[0] - position[0])
+                    -2*(-1)*alpha_2*delta_t*(self.robot.L2*math.cos(selfthetas1+ selfthetas2))*(target[1] - position[1])
+                ]
+                # The two args after grad are the gradient learning steps for t+1 and t
+                # si critere augmente on BP un bruit fction randon_update, sion on BP le gradient
+                
+                if (crit_ap <= crit_av) :
+                    self.network.backPropagate(grad,self.pas,self.moment)# grad, pas d'app, moment
+                else :
+                    #self.network.random_update(0.001)
+                    self.network.backPropagate(grad, self.pas ,self.moment)
+                    
+        #self.robot.train(th1,th2) 
+        #self.robot.set_theta([0,0]) # stop  apres arret  du prog d'app                 # Fonction à changer 
+        #self.robot.train()
+        #position = self.robot.get_position() #  obtient nvlle pos robot instant t+1
+        #Teta_t=position[2]
+             
+                
+        return th1,th2
